60e63114f36dae1b2e4f0430ea7bcd1a396a69a0
gd <hubiao1991012936@gmail.com>
2022-10-31 15:34:20 +0100

    create django flow

--- CreatFactura/CreatFactura/urls.py
+++ CreatFactura/CreatFactura/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app01 import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('index/',views.index),
+    path("user/list/",views.user_list),
+    path("user/add/",views.user_add)
+
 ]
--- CreatFactura/app01/views.py
+++ CreatFactura/app01/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("欢迎使用！！")
+
+
+
+def user_list(request):
+    # 返回页面 render
+    #默认去app 下 找对应的 templates目录中找 (回去所有注册的app中去寻找，按顺序查找)
+    #具体查找目录，优先找配置文件settings 中 TEMPLATES ，dir 寻找
+    return render(request,"user_list.html")
+
+
+def user_add(request):
+    return  HttpResponse("添加用户")
